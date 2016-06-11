83f09c8fab88f4e93e31795e5d93660993261dd0
Steven Benner <admin@stevenbenner.com>
2016-06-11 14:45:40 -0700

    Cleaned up zsh_hooks.zsh.
    Removed redundant window title setting code. All of this should
    now be handled in the updated preexec function.

--- zsh/.zsh/zsh_hooks.zsh
+++ zsh/.zsh/zsh_hooks.zsh
@@ -1,16 +1,3 @@
-function precmd {
-	# vcs_info
-	# Put the string "hostname::/full/directory/path" in the title bar:
-	echo -ne "\e]2;$PWD\a"
-
-	# Put the parentdir/currentdir in the tab
-	echo -ne "\e]1;$PWD:h:t/$PWD:t\a"
-}
-
-function set_running_app {
-	printf "\e]1; $PWD:t:$(history $HISTCMD | cut -b7- ) \a"
-}
-
 function preexec {
 	local a=${${1## *}[(w)1]}  # get the command
 	local b=${a##*\/}          # get the command basename
@@ -20,8 +7,4 @@ function preexec {
 	a=${(V)a//\%/\%\%}         # escape non-visibles and print specials
 
 	print -Pn "\e]2;$a\a"
-}
-
-function postexec {
-	set_running_app
 }
