f7880b84f58d069775853f0b483f0c2b1d658334
Steven Benner <admin@stevenbenner.com>
2016-06-12 13:54:50 -0700

    Cleaned up zsh aliases file.

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -7,15 +7,11 @@ alias chown='chown --preserve-root'
 alias chmod='chmod --preserve-root'
 alias chgrp='chgrp --preserve-root'
 
-# Colorize output, add file type indicator, and put sizes in human readable format
+# colored output and basic tweaks
 alias ls='ls -GFh --group-directories-first --color=auto'
-
-# Same as above, but in long listing format
 alias ll='ls -GFhl --group-directories-first --color=auto'
-
-# Enable colors in grep and ignore VCS folders
 alias grep='grep --color=auto --exclude-dir={.bzr,.cvs,.git,.hg,.svn}'
-
 alias diff='colordiff'
 
+# custom commands
 alias newest='find . \( -path \*/.git -o -path \*/node_modules \) -prune -o -type f -printf "%TY-%Tm-%Td %TT %p\n" | sort -r | less'
