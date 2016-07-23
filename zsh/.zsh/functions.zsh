ae7c0b339a74320a8eb3d3568fe357b0d318a72e
Steven Benner <admin@stevenbenner.com>
2016-07-23 15:36:27 -0700

    Changed zsh extract function to escape paths
    This change lets this function handle file names and paths with
    spaces or special characters.

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -2,22 +2,22 @@ function extract {
 	if [ -f $1 ] ; then
 		echo Extracting $1 ...
 		case $1 in
-			*.7z)       7z x $1       ;;
-			*.bz2)      bunzip2 $1    ;;
-			*.cab)      cabextract $1 ;;
-			*.exe)      cabextract $1 ;;
-			*.gz)       gunzip $1     ;;
-			*.lzma)     unlzma $1     ;;
-			*.rar)      unrar x $1    ;;
-			*.tar)      tar xf $1     ;;
-			*.tar.bz2)  tar xjf $1    ;;
-			*.tar.gz)   tar xzf $1    ;;
-			*.tar.xz)   tar xJf $1    ;;
-			*.tbz2)     tar xjf $1    ;;
-			*.tgz)      tar xzf $1    ;;
-			*.xz)       unxz $1       ;;
-			*.Z)        uncompress $1 ;;
-			*.zip)      unzip $1      ;;
+			*.7z)       7z x ./"$1"       ;;
+			*.bz2)      bunzip2 ./"$1"    ;;
+			*.cab)      cabextract ./"$1" ;;
+			*.exe)      cabextract ./"$1" ;;
+			*.gz)       gunzip ./"$1"     ;;
+			*.lzma)     unlzma ./"$1"     ;;
+			*.rar)      unrar x ./"$1"    ;;
+			*.tar)      tar xf ./"$1"     ;;
+			*.tar.bz2)  tar xjf ./"$1"    ;;
+			*.tar.gz)   tar xzf ./"$1"    ;;
+			*.tar.xz)   tar xJf ./"$1"    ;;
+			*.tbz2)     tar xjf ./"$1"    ;;
+			*.tgz)      tar xzf ./"$1"    ;;
+			*.xz)       unxz ./"$1"       ;;
+			*.Z)        uncompress ./"$1" ;;
+			*.zip)      unzip ./"$1"      ;;
 			*)          echo "'$1' cannot be extracted via extract()" ;;
 		esac
 	else
