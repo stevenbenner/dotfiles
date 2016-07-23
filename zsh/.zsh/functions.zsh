0dda2618f4869ded2bffa7a00f78fce6aea1b926
Steven Benner <admin@stevenbenner.com>
2016-07-23 14:10:58 -0700

    Added xz support to zsh extract function

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -9,8 +9,10 @@ function extract {
 			*.tar)      tar xf $1     ;;
 			*.tar.bz2)  tar xjf $1    ;;
 			*.tar.gz)   tar xzf $1    ;;
+			*.tar.xz)   tar xJf $1    ;;
 			*.tbz2)     tar xjf $1    ;;
 			*.tgz)      tar xzf $1    ;;
+			*.xz)       unxz $1       ;;
 			*.Z)        uncompress $1 ;;
 			*.zip)      unzip $1      ;;
 			*)          echo "'$1' cannot be extracted via extract()" ;;
