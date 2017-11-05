8409ecfc78fd94c01b7d590e1efc8a8b2eb0e9c0
Steven Benner <admin@stevenbenner.com>
2017-11-04 17:50:07 -0700

    Added freebsd aliases to zsh config

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -3,14 +3,31 @@ alias cp='cp -i'
 alias mv='mv -i'
 alias ln='ln -i'
 alias rm='rm -I'
-alias chown='chown --preserve-root'
-alias chmod='chmod --preserve-root'
-alias chgrp='chgrp --preserve-root'
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	alias chown='chown --preserve-root'
+	alias chmod='chmod --preserve-root'
+	alias chgrp='chgrp --preserve-root'
+elif [[ "$OSTYPE" == "freebsd"* ]] && type gchown > /dev/null 2>&1; then
+	alias chown='gchown --preserve-root'
+	alias chmod='gchmod --preserve-root'
+	alias chgrp='gchgrp --preserve-root'
+fi
 
 # colored output and basic tweaks
-alias ls='ls -GFh --group-directories-first --color=auto'
-alias ll='ls -GFhl --group-directories-first --color=auto'
-alias grep='grep --color=auto --exclude-dir={.bzr,.cvs,.git,.hg,.svn}'
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	alias ls='ls -GFh --group-directories-first --color=auto'
+	alias ll='ls -GFhl --group-directories-first --color=auto'
+	alias grep='grep --color=auto --exclude-dir={.bzr,.cvs,.git,.hg,.svn}'
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+	if type gls > /dev/null 2>&1; then
+		alias ls='gls -GFh --group-directories-first --color=auto'
+		alias ll='gls -GFhl --group-directories-first --color=auto'
+	else
+		alias ls='ls -GFh'
+		alias ll='ls -GFhl'
+	fi
+	alias grep='grep --color=auto --exclude={.bzr,.cvs,.git,.hg,.svn}'
+fi
 alias diff='colordiff'
 alias ip='ip -color'
 
