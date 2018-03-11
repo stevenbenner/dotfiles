7348f2f11de2d9e74821b9622e3fee2c390ff09a
Steven Benner <admin@stevenbenner.com>
2018-03-10 17:36:27 -0800

    Moved npmrc file to XDG_CONFIG_HOME/npm folder

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -4,3 +4,4 @@ export P4CONFIG=.p4config
 # xdg directories
 export LESSHISTFILE="$XDG_CACHE_HOME/lesshst"
 export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
