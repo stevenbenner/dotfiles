731ca526273a576d802420c6256620679668403b
Steven Benner <admin@stevenbenner.com>
2018-03-17 16:04:28 -0700

    Moved cargo data directory to XDG_DATA_HOME/cargo folder

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -2,6 +2,7 @@ export LESS='--ignore-case --raw-control-chars'
 export P4CONFIG=.p4config
 
 # xdg directories
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
 export LESSHISTFILE="$XDG_CACHE_HOME/lesshst"
 export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
 export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
