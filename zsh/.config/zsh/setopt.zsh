d59ee8465cc0192bb740d6c537204ee83d2b8d31
Steven Benner <admin@stevenbenner.com>
2018-03-31 22:57:02 -0700

    Clean up comments in zsh setopt script

--- zsh/.config/zsh/setopt.zsh
+++ zsh/.config/zsh/setopt.zsh
@@ -1,50 +1,22 @@
-# ===== Basics
+# basics
+setopt AUTO_CD # if you type foo, and it isn't a command, and it is a directory in your cdpath, go there
 
-# If you type foo, and it isn't a command, and it is a directory in your cdpath, go there
-setopt AUTO_CD
+# history
+setopt APPEND_HISTORY # allow multiple terminal sessions to all append to one zsh command history
+setopt EXTENDED_HISTORY # include more information about when the command was executed, etc
+setopt HIST_FIND_NO_DUPS # when searching history don't display results already cycled through twice
+setopt HIST_IGNORE_DUPS # do not write events to history that are duplicates of previous events
+setopt HIST_IGNORE_ALL_DUPS # do not write events to history that are duplicates of previous events
+setopt HIST_IGNORE_SPACE # do not save history for commanding beginning with a space
+setopt HIST_REDUCE_BLANKS # remove extra blanks from each command line being added to history
+setopt INC_APPEND_HISTORY # add comamnds as they are typed, don't wait until shell exit
 
-# Allow comments even in interactive shells (especially for Muness)
-# setopt INTERACTIVE_COMMENTS
+# completion
+setopt ALWAYS_TO_END # when completing from the middle of a word, move the cursor to the end of the word
+setopt COMPLETE_IN_WORD # allow completion from within a word/phrase
+setopt LIST_PACKED # make the completion list take less lines with variable width columns
 
-# ===== History
-
-# Allow multiple terminal sessions to all append to one zsh command history
-setopt APPEND_HISTORY
-
-# Add comamnds as they are typed, don't wait until shell exit
-setopt INC_APPEND_HISTORY
-
-# Do not write events to history that are duplicates of previous events
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_ALL_DUPS
-
-# Do not save history for commanding beginning with a space
-setopt HIST_IGNORE_SPACE
-
-# When searching history don't display results already cycled through twice
-setopt HIST_FIND_NO_DUPS
-
-# Remove extra blanks from each command line being added to history
-setopt HIST_REDUCE_BLANKS
-
-# Include more information about when the command was executed, etc
-setopt EXTENDED_HISTORY
-
-# ===== Completion
-
-# Allow completion from within a word/phrase
-setopt COMPLETE_IN_WORD
-
-# When completing from the middle of a word, move the cursor to the end of the word
-setopt ALWAYS_TO_END
-
-# Make the completion list take less lines with variable width columns
-setopt LIST_PACKED
-
-# ===== Prompt
-
-# Enable parameter expansion, command substitution, and arithmetic expansion in the prompt
-setopt PROMPT_SUBST
-
-unsetopt MENU_COMPLETE
-setopt AUTO_MENU
+# prompt
+setopt AUTO_MENU # automatically use menu completion after the second consecutive request for completion
+unsetopt MENU_COMPLETE # force the list display on ambiguious matches instead of selecting the first match
+setopt PROMPT_SUBST # enable parameter expansion, command substitution, and arithmetic expansion in the prompt
