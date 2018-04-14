492103d383f547daecb5fc4eaa90cb052b9d6fec
Steven Benner <admin@stevenbenner.com>
2018-04-14 03:43:59 -0700

    Simplified git_prompt_info function in zsh prompt script

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -1,25 +1,24 @@
 function git_prompt_info {
 	local ref=$(=git symbolic-ref HEAD 2> /dev/null)
 	local gitst="$(=git status 2> /dev/null)"
+	local gitstatus
 
 	if [[ -f .git/MERGE_HEAD ]]; then
 		if [[ ${gitst} =~ "unmerged" ]]; then
-			gitstatus=" %F{red}unmerged%f"
+			gitstatus="%F{red}unmerged%f"
 		else
-			gitstatus=" %F{green}merged%f"
+			gitstatus="%F{green}merged%f"
 		fi
 	elif [[ ${gitst} =~ "Changes to be committed" ]]; then
-		gitstatus=" %F{blue}%B!%b%f"
+		gitstatus="%F{blue}%B!%b%f"
 	elif [[ ${gitst} =~ "use \"git add" ]]; then
-		gitstatus=" %F{red}%B!%b%f"
+		gitstatus="%F{red}%B!%b%f"
 	elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
-		gitstatus=" %F{yellow}%B*%b%f"
-	else
-		gitstatus=' '
+		gitstatus="%F{yellow}%B*%b%f"
 	fi
 
 	if [[ -n $ref ]]; then
-		echo "%F{green}[%B${ref#refs/heads/}%b]%f$gitstatus"
+		echo "%F{green}[%B${ref#refs/heads/}%b]%f $gitstatus"
 	elif [[ ${gitst} =~ "HEAD detached at" ]]; then
 		echo "%F{red}[%Bdetached%b]%f "
 	fi
