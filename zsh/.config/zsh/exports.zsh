e005d327b037e1c009eb5822ebc7e81f00e620c3
Steven Benner <admin@stevenbenner.com>
2018-04-21 05:09:35 -0700

    Moved gem directories to XDG_DATA_HOME/gem and XDG_CACHE_HOME/gem

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -3,6 +3,8 @@ export P4CONFIG='.p4config'
 
 # xdg directories
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export GEM_HOME="$XDG_DATA_HOME/gem"
+export GEM_SPEC_CACHE="$XDG_CACHE_HOME/gem"
 export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
 export LESSHISTFILE="$XDG_CACHE_HOME/lesshst"
 export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
