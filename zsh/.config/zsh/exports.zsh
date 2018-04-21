fe5a3612622e568379411c6f3563a5240520d96d
Steven Benner <admin@stevenbenner.com>
2018-04-21 04:51:57 -0700

    Moved gradle user directory to XDG_DATA_HOME/gradle folder

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -3,6 +3,7 @@ export P4CONFIG='.p4config'
 
 # xdg directories
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
 export LESSHISTFILE="$XDG_CACHE_HOME/lesshst"
 export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
 export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
