cadbddf75a5800efdf68122949c4ce16530c7e82
Steven Benner <admin@stevenbenner.com>
2018-04-28 09:01:43 -0700

    Fixed case ordering in zsh extract function

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -3,16 +3,16 @@ function extract {
 		echo "Extracting $1 ..."
 		case $1 in
 			*.7z)       7z x ./"$1"       ;;
-			*.bz2)      bunzip2 ./"$1"    ;;
 			*.cab)      cabextract ./"$1" ;;
 			*.exe)      cabextract ./"$1" ;;
-			*.gz)       gunzip ./"$1"     ;;
 			*.lzma)     unlzma ./"$1"     ;;
 			*.rar)      unrar x ./"$1"    ;;
-			*.tar)      tar xf ./"$1"     ;;
 			*.tar.bz2)  tar xjf ./"$1"    ;;
+			*.bz2)      bunzip2 ./"$1"    ;;
 			*.tar.gz)   tar xzf ./"$1"    ;;
+			*.gz)       gunzip ./"$1"     ;;
 			*.tar.xz)   tar xJf ./"$1"    ;;
+			*.tar)      tar xf ./"$1"     ;;
 			*.tbz2)     tar xjf ./"$1"    ;;
 			*.tgz)      tar xzf ./"$1"    ;;
 			*.xz)       unxz ./"$1"       ;;
