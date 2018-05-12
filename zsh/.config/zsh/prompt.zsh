289ca2012c1991695bed65234853f5ece5d2a92f
Steven Benner <admin@stevenbenner.com>
2018-05-12 08:39:57 -0700

    Replace unnecessary double-quotes in zsh git prompt script

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -4,14 +4,14 @@ function git_prompt_info {
 	local gitstatus
 
 	if [[ -f .git/MERGE_HEAD ]]; then
-		if [[ ${gitst} =~ "unmerged" ]]; then
+		if [[ ${gitst} =~ 'unmerged' ]]; then
 			gitstatus="%F{red}unmerged%f"
 		else
 			gitstatus="%F{green}merged%f"
 		fi
-	elif [[ ${gitst} =~ "Changes to be committed" ]]; then
+	elif [[ ${gitst} =~ 'Changes to be committed' ]]; then
 		gitstatus="%F{blue}%B!%b%f"
-	elif [[ ${gitst} =~ "use \"git add" ]]; then
+	elif [[ ${gitst} =~ 'use "git add' ]]; then
 		gitstatus="%F{red}%B!%b%f"
 	elif [[ -n $(git checkout HEAD 2> /dev/null | grep ahead) ]]; then
 		gitstatus="%F{yellow}%B*%b%f"
@@ -19,7 +19,7 @@ function git_prompt_info {
 
 	if [[ -n $ref ]]; then
 		echo "%F{green}[%B${ref#refs/heads/}%b]%f $gitstatus"
-	elif [[ ${gitst} =~ "HEAD detached at" ]]; then
+	elif [[ ${gitst} =~ 'HEAD detached at' ]]; then
 		echo "%F{red}[%Bdetached%b]%f "
 	fi
 }
