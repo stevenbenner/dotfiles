06ad3b8e0d7821447fc7c4971afb51e1ebd053c6
Steven Benner <admin@stevenbenner.com>
2023-10-28 20:59:22 -0700

    zsh: Reformat `case` statement in `extract` function

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,24 +1,35 @@
 function extract {
 	if [ -f "$1" ] ; then
 		echo "Extracting $1 ..."
-		case $1 in
-			*.7z)       7z x ./"$1"       ;;
-			*.cab)      cabextract ./"$1" ;;
-			*.exe)      cabextract ./"$1" ;;
-			*.lzma)     unlzma ./"$1"     ;;
-			*.rar)      unrar x ./"$1"    ;;
-			*.tar.bz2)  tar xjf ./"$1"    ;;
-			*.bz2)      bunzip2 ./"$1"    ;;
-			*.tar.gz)   tar xzf ./"$1"    ;;
-			*.gz)       gunzip ./"$1"     ;;
-			*.tar.xz)   tar xJf ./"$1"    ;;
-			*.tar)      tar xf ./"$1"     ;;
-			*.tbz2)     tar xjf ./"$1"    ;;
-			*.tgz)      tar xzf ./"$1"    ;;
-			*.xz)       unxz ./"$1"       ;;
-			*.Z)        uncompress ./"$1" ;;
-			*.zip)      unzip ./"$1"      ;;
-			*)          echo "'$1' cannot be extracted via extract()" ;;
+		case "$1" in
+			*.tar.bz2|*.tbz2)
+				tar xjf "./$1" ;;
+			*.tar.gz|*.tgz)
+				tar xzf "./$1" ;;
+			*.tar.xz)
+				tar xJf "./$1" ;;
+			*.7z)
+				7z x "./$1" ;;
+			*.bz2)
+				bunzip2 "./$1" ;;
+			*.cab|*.exe)
+				cabextract "./$1" ;;
+			*.gz)
+				gunzip "./$1" ;;
+			*.lzma)
+				unlzma "./$1" ;;
+			*.rar)
+				unrar x "./$1" ;;
+			*.tar)
+				tar xf "./$1" ;;
+			*.xz)
+				unxz "./$1" ;;
+			*.Z)
+				uncompress "./$1" ;;
+			*.zip)
+				unzip "./$1" ;;
+			*)
+				echo "'$1' cannot be extracted via extract()" ;;
 		esac
 	else
 		echo "'$1' is not a valid file"
