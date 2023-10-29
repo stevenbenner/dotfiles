853086cc840208e8358183a2beee04529d09b4bb
Steven Benner <admin@stevenbenner.com>
2023-10-28 21:11:48 -0700

    zsh: Move `extract` file exists check to top of function

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,42 +1,43 @@
 function extract {
-	if [ -f "$1" ] ; then
-		echo "Extracting $1 ..."
-		case "$1" in
-			*.tar.bz2|*.tbz2)
-				tar xjf "./$1" ;;
-			*.tar.gz|*.tgz)
-				tar xzf "./$1" ;;
-			*.tar.xz)
-				tar xJf "./$1" ;;
-			*.7z)
-				7z x "./$1" ;;
-			*.bz2)
-				bunzip2 "./$1" ;;
-			*.cab|*.exe)
-				cabextract "./$1" ;;
-			*.gz)
-				gunzip "./$1" ;;
-			*.lzma)
-				unlzma "./$1" ;;
-			*.rar)
-				unrar x "./$1" ;;
-			*.tar)
-				tar xf "./$1" ;;
-			*.xz)
-				unxz "./$1" ;;
-			*.Z)
-				uncompress "./$1" ;;
-			*.zip)
-				unzip "./$1" ;;
-			*)
-				echo "'$1' cannot be extracted via extract()"
-				return 1
-				;;
-		esac
-	else
+	if [ ! -f "$1" ]; then
 		echo "'$1' is not a valid file"
 		return 1
 	fi
+
+	echo "Extracting $1 ..."
+
+	case "$1" in
+		*.tar.bz2|*.tbz2)
+			tar xjf "./$1" ;;
+		*.tar.gz|*.tgz)
+			tar xzf "./$1" ;;
+		*.tar.xz)
+			tar xJf "./$1" ;;
+		*.7z)
+			7z x "./$1" ;;
+		*.bz2)
+			bunzip2 "./$1" ;;
+		*.cab|*.exe)
+			cabextract "./$1" ;;
+		*.gz)
+			gunzip "./$1" ;;
+		*.lzma)
+			unlzma "./$1" ;;
+		*.rar)
+			unrar x "./$1" ;;
+		*.tar)
+			tar xf "./$1" ;;
+		*.xz)
+			unxz "./$1" ;;
+		*.Z)
+			uncompress "./$1" ;;
+		*.zip)
+			unzip "./$1" ;;
+		*)
+			echo "'$1' cannot be extracted via extract()"
+			return 1
+			;;
+	esac
 }
 
 function git_pull_all {
