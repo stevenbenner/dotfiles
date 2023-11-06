de925387872dfb7a4d160b7a40401d6e06de30bd
Steven Benner <admin@stevenbenner.com>
2023-11-06 02:07:19 -0800

    zsh: Remove redundant escapes in zsh hooks

--- zsh/.config/zsh/zsh_hooks.zsh
+++ zsh/.config/zsh/zsh_hooks.zsh
@@ -1,6 +1,6 @@
 function precmd {
 	# print working directory to title
-	print -Pn "\\e]2;$PWD\\a"
+	print -Pn "\e]2;$PWD\a"
 }
 
 function preexec {
@@ -8,10 +8,10 @@ function preexec {
 
 	trimarg="${1#"${1%%[![:space:]]*}"}"  # trim leading whitespace
 	cmd=${trimarg[(w)1]}                  # get the command
-	basecmd=${cmd##*\/}                   # get the command basename
+	basecmd=${cmd##*/}                    # get the command basename
 	cmd="${basecmd}${trimarg#"$cmd"}"     # add back the parameters
 	cmd=$(print -Pn "$cmd" | tr -d '\t\n\v\f\r')  # remove fancy whitespace
 	cmd=${(V)cmd//\%/\%\%}                # escape non-visibles and print specials
 
-	print -Pn "\\e]2;$cmd\\a"
+	print -Pn "\e]2;$cmd\a"
 }
