86573268e8ec10f7eb15aea9df1b5c97b1b41e10
Steven Benner <admin@stevenbenner.com>
2025-06-03 12:29:22 -0700

    zsh: Remove --no-group option from ls aliases that are not -l
    The --no-group/-G option only applies to long format output.

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -17,12 +17,12 @@ fi
 
 # colored output and basic tweaks
 if [[ "$OSTYPE" = "linux-gnu"* ]]; then
-	alias ls='ls --no-group --classify --human-readable --literal --group-directories-first --color=auto'
+	alias ls='ls --classify --human-readable --literal --group-directories-first --color=auto'
 	alias ll='ls --no-group --classify --human-readable --literal --group-directories-first --color=auto -l'
 	alias grep='grep --color=auto --exclude-dir={.bzr,.cvs,.git,.hg,.svn}'
 elif [[ "$OSTYPE" = "freebsd"* ]]; then
 	if type gls > /dev/null 2>&1; then
-		alias ls='gls --no-group --classify --human-readable --literal --group-directories-first --color=auto'
+		alias ls='gls --classify --human-readable --literal --group-directories-first --color=auto'
 		alias ll='gls --no-group --classify --human-readable --literal --group-directories-first --color=auto -l'
 	else
 		alias ls='ls -GF'
